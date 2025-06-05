d72e41b1c917d81fe8685c7080643622f75b0058
Helsingbuss <info@helsingbuss.se>
2025-06-05 22:07:55 +0200

    Fix: vite input pekar rätt på public/index.html

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  root: '.', // <--- LÄGG TILL DETTA FÖR TYDLIG STARTPUNKT
   plugins: [react()],
   build: {
-    outDir: 'dist' // viktigt att den matchar Vercels Output Directory
+    outDir: 'dist',
+    rollupOptions: {
+      input: 'public/index.html' // <--- Detta säger åt Vite att börja här
+    }
   }
 })
