04614f3a2f71de185ac8c2a360745f7e6279e94a
Helsingbuss <info@helsingbuss.se>
2025-06-05 21:49:12 +0200

    Fix: flyttade public-mapp från src/ till root/

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  root: '.', // <-- här är ändringen
+  root: '.', // behövs när index.html ligger i roten
   plugins: [react()],
   build: {
     outDir: 'dist'
   }
 })
-
