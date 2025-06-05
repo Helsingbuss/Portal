ef4ccc3736d7b745c22fb0d34f75de677c6a3f84
Helsingbuss <info@helsingbuss.se>
2025-06-05 22:05:48 +0200

    Deploy: korrekt struktur + design återställd

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  root: '.', // behövs när index.html ligger i roten
   plugins: [react()],
   build: {
-    outDir: 'dist'
+    outDir: 'dist' // viktigt att den matchar Vercels Output Directory
   }
 })
