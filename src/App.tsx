ca0b7194273f10eddebb4e98e12ff339c443cb07
Helsingbuss <info@helsingbuss.se>
2025-06-24 15:21:16 +0200

    Fix: byter App till RoutesWrapper, routes hanterar inloggning

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,4 @@
 import React from "react";
-import Login from "./pages/Login";
-
-const App: React.FC = () => (
-  <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-100 to-blue-300">
-    <Login />
-  </div>
-);
-
+import RoutesWrapper from "./Routes";
+const App: React.FC = () => <RoutesWrapper />;
 export default App;
