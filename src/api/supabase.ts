17e6d58fa29ba19381c2659112f6e94759145f7b
Helsingbuss <info@helsingbuss.se>
2025-06-24 14:16:08 +0200

    Fix: postcss config till CommonJS, build/white-screen bug

--- src/api/supabase.ts
+++ src/api/supabase.ts
@@ -1,8 +1,6 @@
-// /src/api/supabase.ts
+import { createClient } from "@supabase/supabase-js";
 
-import { createClient } from '@supabase/supabase-js';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY as string;
 
-const supabaseUrl = 'https://gfjchjixpsfgxoprdsfx.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdmamNoaml4cHNmZ3hvcHJkc2Z4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkxMjYyMjMsImV4cCI6MjA2NDcwMjIyM30.MAl-fl15C6ua-CL3-76UOCwmWs0smNapTvMLmwvT8MQ';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseKey);
