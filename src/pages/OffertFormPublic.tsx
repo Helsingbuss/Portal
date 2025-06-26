a5d0816560428f1bdb82bc4993e654cf673f374b
Helsingbuss <info@helsingbuss.se>
2025-06-26 20:50:09 +0200

    Publik offertformulär på /offertform för WordPress iframe

---
+++ src/pages/OffertFormPublic.tsx
@@ -0,0 +1,10 @@
+// /src/pages/OffertFormPublic.tsx
+import OffertForm from '../components/OffertForm';
+
+export default function OffertFormPublic() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-100 to-blue-300">
+      <OffertForm />
+    </div>
+  );
+}
